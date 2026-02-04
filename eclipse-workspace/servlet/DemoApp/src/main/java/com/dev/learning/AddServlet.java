1ed1b4c6ce14ad3612bf0681efb4e247b074bbd7
Saurabh <devsaurabh086@gmail.com>
2026-02-04 23:03:45 +0530

    Calling a Servlet Inside a Servlet via RequestDispatcher

--- eclipse-workspace/servlet/DemoApp/src/main/java/com/dev/learning/AddServlet.java
+++ eclipse-workspace/servlet/DemoApp/src/main/java/com/dev/learning/AddServlet.java
@@ -2,17 +2,21 @@ package com.dev.learning;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 
 public class AddServlet extends HttpServlet {
 	
-	public void doPost(HttpServletRequest req , HttpServletResponse res ) throws IOException {
+	public void doGet(HttpServletRequest req , HttpServletResponse res ) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int result = i+j;
-		System.out.println(result);
-		res.getWriter().println(result);
+		int k = result * result;
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
 		
 	}
 
---
+++ eclipse-workspace/servlet/DemoApp/src/main/java/com/dev/learning/SquareServlet.java
@@ -0,0 +1,13 @@
+package com.dev.learning;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SquareServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req , HttpServletResponse res ) throws IOException {
+    	res.getWriter().println("hii there , this is square servlet");
+    }
+}
