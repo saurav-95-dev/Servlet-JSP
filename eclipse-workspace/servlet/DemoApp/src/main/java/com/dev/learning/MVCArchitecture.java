39c0e2e9f9e5becd0e62cd9f12931ccb35d1c3c8
Saurabh <devsaurabh086@gmail.com>
2026-02-08 20:18:21 +0530

    MVC Architecture in Servlet & JSP

---
+++ eclipse-workspace/servlet/DemoApp/src/main/java/com/dev/learning/MVCArchitecture.java
@@ -0,0 +1,14 @@
+//MVC DESIGN PATTERN USING SERVLET AND JSP :
+//ARCHITECTURE : 
+//What's the problem with JSP ? 
+//→ When we run JSP for the first time , it get's converted into servlet which takes time . 
+//→ We can't/should never write our business logic inside JSP because it's only for the view . That means all the information processing stuff should take place inside the servlet itself . 
+//→ Through html , we can only create static pages , but with JSP we can go for dynamic pages.
+//
+//ARCHITCTURE FLOW : 
+//→ Layout ( Design ) → Will be done through JSP → It will be just a layout without data.
+//→ Client will send a request for a data let's say - /getpage  → request goes to →  CONTROLLER
+//→ Now this CONTROLLER (created with the help of servlets) holds the data in the format of MODEL OBJECT (POJO-Plain old java object)and it's job is to send the this model object to the layout/view/JSP and  call it  .
+//→CONTROLLER only only for accepting request and sending response , so we dont perform actual processing inside controller . 
+//→To perform data processing , this CONTROLLER will another class called Service Class.
+//→Inside this Service Class , we can write JDBC steps to connect it to DB via DAO Layer/classes.
