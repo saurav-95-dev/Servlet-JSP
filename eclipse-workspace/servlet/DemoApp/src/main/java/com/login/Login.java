2b25e5415e3481ef29fb22496d411dd1cedca50a
Saurabh <devsaurabh086@gmail.com>
2026-03-01 16:51:58 +0530

    Eastablising JDBC Connection

--- eclipse-workspace/servlet/DemoApp/src/main/java/com/login/Login.java
+++ eclipse-workspace/servlet/DemoApp/src/main/java/com/login/Login.java
@@ -8,6 +8,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import com.login.dao.LoginDao;
 
 
 @WebServlet("/Login")
@@ -16,13 +19,20 @@ public class Login extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String uname = request.getParameter("uname");
 		String password = request.getParameter(("pass"));
-		if(uname.equals("Saurabh") && password.equals("Saurabh@123")) {
-			HttpSession session = request.getSession();
-			session.setAttribute("username", uname);
-			response.sendRedirect("welcome.jsp");
-		}
-		else {
-			response.sendRedirect("login.jsp");
+		
+		try {
+			LoginDao dao = new LoginDao();
+			if(dao.checkCredentials(uname, password)) {
+				HttpSession session = request.getSession();
+				session.setAttribute("username", uname);
+				response.sendRedirect("welcome.jsp");
+			}
+			else {
+				response.sendRedirect("login.jsp");
+			}
+		} catch (SQLException | IOException e) {
+			
+			e.printStackTrace();
 		}
 	}
 
--- eclipse-workspace/servlet/DemoApp/src/main/java/com/login/dao/loginDao.java
+++ eclipse-workspace/servlet/DemoApp/src/main/java/com/login/dao/loginDao.java
@@ -1,36 +1,32 @@
 package com.login.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
-public class loginDao {
-	String query = "select * from login where uname= ? and pass = ?";
-	
-	String url = "jdbc:mysql://localhost:3306/org";
-	String username = "root";
-	String password = "kuch@123";
-	public boolean checkCredentials(String  uname, String pass) throws SQLException {
-		try {
-			Class.forName("com.mysql.jdbc.driver");
-			Connection con = DriverManager.getConnection(url , username , password);
-			PreparedStatement st = con.prepareStatement(query);
-			st.setString(1, uname);
-			st.setString(2, pass);
-			ResultSet rs = st.executeQuery();
-			if(rs.next()) {
-				return true;
-			}
-			
-			
-		} catch (ClassNotFoundException e) {
-			
-			e.printStackTrace();
-		}
-		
-		return false;
-	}
+public class LoginDao {
 
+    String query = "select * from login where uname=? and pass=?";
+    String url = "jdbc:mysql://localhost:3306/org";
+    String username = "root";
+    String password = "kuch@123";
+
+    public boolean checkCredentials(String uname, String pass) throws SQLException {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // ✅ FIXED
+
+            Connection con = DriverManager.getConnection(url, username, password);
+            PreparedStatement st = con.prepareStatement(query);
+
+            st.setString(1, uname);
+            st.setString(2, pass);
+
+            ResultSet rs = st.executeQuery();
+            return rs.next(); // cleaner
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
 }
